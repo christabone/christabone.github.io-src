1189e1efaf6e5947af0008e21c6c46e45955a20c
Chris Tabone <christabone@gmail.com>
2019-03-12 20:33:32 -0300

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,9 @@ SITELOGO = SITEURL + '/images/photo.jpg'
 
 PATH = '/users/ctabone/Programming/christabone.github.io-src/content'
 
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
 TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
